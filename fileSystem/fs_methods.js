1999794a9e32f341a3b768b3a04e185a9100899a
lauren brett <laurencareybrett@gmail.com>
2020-07-10 15:27:26 +0100

    overwrite text in file

--- fileSystem/fs_methods.js
+++ fileSystem/fs_methods.js
@@ -1,15 +1,33 @@
 let fs = require("fs");
 
-fs.appendFile(
-  "newFile.txt",
-  "This is the new content that now shows in the new file",
+// fs.appendFile(
+//   "newFile.txt",
+//   "This is the new content that now shows in the new file",
+//   function (err) {
+//     if (err) throw err;
+//     console.log("saving");
+//   }
+// );
+
+// fs.writeFile("newFileWritten.txt", "words are being saved!", function (err) {
+//   if (err) throw err;
+//   console.log("this works");
+// });
+
+// fs.appendFile(
+//   "newFile.txt",
+//   " ...add this to the file in a second append",
+//   function (err) {
+//     if (err) throw err;
+//     console.log("updated with more text");
+//   }
+// );
+
+fs.writeFile(
+  "overwrittenFile.txt",
+  "overwritten what was here, this is new text",
   function (err) {
     if (err) throw err;
-    console.log("saving");
+    console.log("changed text");
   }
 );
-
-fs.writeFile("newFileWritten.txt", "words are being saved!", function (err) {
-  if (err) throw err;
-  console.log("this works");
-});
